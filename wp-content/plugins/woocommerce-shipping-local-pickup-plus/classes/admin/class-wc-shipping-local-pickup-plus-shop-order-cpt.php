98915a5ea7ed56c32b439e7ccac2c571e9fb5892
33Evgenyy33 <33Evgenyy33@gmail.com>
2017-06-05 00:15:27 +0300

    POS

--- wp-content/plugins/woocommerce-shipping-local-pickup-plus/classes/admin/class-wc-shipping-local-pickup-plus-shop-order-cpt.php
+++ wp-content/plugins/woocommerce-shipping-local-pickup-plus/classes/admin/class-wc-shipping-local-pickup-plus-shop-order-cpt.php
@@ -22,7 +22,7 @@
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
  */
 
-defined( 'ABSPATH' ) or exit;
+defined('ABSPATH') or exit;
 
 /**
  * Local Pickup Plus order custom post type class
@@ -34,168 +34,175 @@ defined( 'ABSPATH' ) or exit;
  *
  * @since 1.8.0
  */
-class WC_Shipping_Local_Pickup_Plus_CPT {
+class WC_Shipping_Local_Pickup_Plus_CPT
+{
 
 
-	/**
-	 * Add actions/filters for View Orders/Edit Order screen
-	 *
-	 * @since 1.8.0
-	 */
-	public function __construct() {
+    /**
+     * Add actions/filters for View Orders/Edit Order screen
+     *
+     * @since 1.8.0
+     */
+    public function __construct()
+    {
 
-		// add 'Pickup Locations' orders page column header
-		add_filter( 'manage_edit-shop_order_columns',        array( $this, 'render_pickup_locations_column_header' ), 20 );
-		// add 'Pickup Locations' orders page column content
-		add_action( 'manage_shop_order_posts_custom_column', array( $this, 'render_pickup_locations_column_content' ) );
-		// add CSS to tweak the 'Pickup Locations' column
-		add_action( 'admin_head',                            array( $this, 'render_pickup_locations_column_styles' ) );
+        // add 'Pickup Locations' orders page column header
+        add_filter('manage_edit-shop_order_columns', array($this, 'render_pickup_locations_column_header'), 20);
+        // add 'Pickup Locations' orders page column content
+        add_action('manage_shop_order_posts_custom_column', array($this, 'render_pickup_locations_column_content'));
+        // add CSS to tweak the 'Pickup Locations' column
+        add_action('admin_head', array($this, 'render_pickup_locations_column_styles'));
 
-		// Filter the keys that should be hidden from order edit screens.
-		add_filter( 'woocommerce_hidden_order_itemmeta', array( $this, 'filter_order_hidden_meta' ) );
-	}
-
-
-	/**
-	 * Hide 'pickup_location' meta array from order edit screens.
-	 *
-	 * @internal
-	 *
-	 * @since 1.14.0
-	 * @param $hidden_meta
-	 * @return array
-	 */
-	public function filter_order_hidden_meta( $hidden_meta ) {
-
-		if ( SV_WC_Plugin_Compatibility::is_wc_version_gte_3_0() ) {
-			$hidden_meta[] = 'pickup_location';
-		}
-
-		return $hidden_meta;
-	}
+        // Filter the keys that should be hidden from order edit screens.
+        add_filter('woocommerce_hidden_order_itemmeta', array($this, 'filter_order_hidden_meta'));
+    }
 
 
-	/** Listable Columns ******************************************************/
+    /**
+     * Hide 'pickup_location' meta array from order edit screens.
+     *
+     * @internal
+     *
+     * @since 1.14.0
+     * @param $hidden_meta
+     * @return array
+     */
+    public function filter_order_hidden_meta($hidden_meta)
+    {
+
+        if (SV_WC_Plugin_Compatibility::is_wc_version_gte_3_0()) {
+            $hidden_meta[] = 'pickup_location';
+        }
+
+        return $hidden_meta;
+    }
 
 
-	/**
-	 * Adds 'Pickup Locations' column header to 'Orders' page
-	 * immediately after 'Ship to' column
-	 *
-	 * @since 1.8.0
-	 * @param array $columns
-	 * @return array $new_columns
-	 */
-	public function render_pickup_locations_column_header( $columns ) {
-
-		$new_columns = array();
-
-		foreach ( $columns as $column_name => $column_info ) {
-
-			$new_columns[ $column_name ] = $column_info;
-
-			if ( 'shipping_address' === $column_name ) {
-
-				$new_columns['pickup_locations'] = __( 'Pickup Locations', 'woocommerce-shipping-local-pickup-plus' );
-			}
-		}
-
-		return $new_columns;
-	}
+    /** Listable Columns ******************************************************/
 
 
-	/**
-	 * Adds 'Pickup Locations' column content to 'Orders' page immediately after 'Order Status' column
-	 *
-	 * @since 1.8.0
-	 * @param array $column name of column being displayed
-	 */
-	public function render_pickup_locations_column_content( $column ) {
-		global $post;
+    /**
+     * Adds 'Pickup Locations' column header to 'Orders' page
+     * immediately after 'Ship to' column
+     *
+     * @since 1.8.0
+     * @param array $columns
+     * @return array $new_columns
+     */
+    public function render_pickup_locations_column_header($columns)
+    {
 
-		if ( 'pickup_locations' === $column ) {
+        $new_columns = array();
 
-			$order = wc_get_order( $post->ID );
+        foreach ($columns as $column_name => $column_info) {
 
-			$pickup_locations = $this->get_order_pickup_locations( $order );
+            $new_columns[$column_name] = $column_info;
 
-			foreach ( $pickup_locations as $pickup_location ) {
+            if ('shipping_address' === $column_name) {
 
-				$formatted_pickup_location = WC()->countries->get_formatted_address( array_merge( array( 'first_name' => null, 'last_name' => null, 'state' => null ), $pickup_location ) );
+                $new_columns['pickup_locations'] = __('Pickup Locations', 'woocommerce-shipping-local-pickup-plus');
+            }
+        }
 
-				if ( isset( $pickup_location['phone'] ) && $pickup_location['phone'] ) {
-					$formatted_pickup_location .= "<br/>\n" . $pickup_location['phone'];
-				}
-
-				echo esc_html( preg_replace( '#<br\s*/?>#i', ', ', $formatted_pickup_location ) );
-			}
-		}
-	}
+        return $new_columns;
+    }
 
 
-	/**
-	 * Adds CSS to style the 'Pickup Locations' column
-	 *
-	 * @since 1.10.1
-	 */
-	public function render_pickup_locations_column_styles() {
+    /**
+     * Adds 'Pickup Locations' column content to 'Orders' page immediately after 'Order Status' column
+     *
+     * @since 1.8.0
+     * @param array $column name of column being displayed
+     */
+    public function render_pickup_locations_column_content($column)
+    {
+        global $post;
 
-		$screen = get_current_screen();
+        if ('pickup_locations' === $column) {
 
-		if ( 'edit-shop_order' === $screen->id ) :
+            $order = wc_get_order($post->ID);
 
-			?>
-			<style type="text/css">
-				.widefat .column-pickup_locations {
-					width: 12%;
-				}
-			</style>
-			<?php
+            $pickup_locations = $this->get_order_pickup_locations($order);
 
-		endif;
-	}
+            foreach ($pickup_locations as $pickup_location) {
+
+                $formatted_pickup_location = WC()->countries->get_formatted_address(array_merge(array('first_name' => null, 'last_name' => null, 'state' => null), $pickup_location));
+
+                if (isset($pickup_location['phone']) && $pickup_location['phone']) {
+                    $formatted_pickup_location .= "<br/>\n" . $pickup_location['phone'];
+                }
+
+                echo esc_html(preg_replace('#<br\s*/?>#i', ', ', $formatted_pickup_location));
+            }
+        }
+    }
 
 
-	/** Helper Methods ********************************************************/
+    /**
+     * Adds CSS to style the 'Pickup Locations' column
+     *
+     * @since 1.10.1
+     */
+    public function render_pickup_locations_column_styles()
+    {
+
+        $screen = get_current_screen();
+
+        if ('edit-shop_order' === $screen->id) :
+
+            ?>
+            <style type="text/css">
+                .widefat .column-pickup_locations {
+                    width: 12%;
+                }
+            </style>
+            <?php
+
+        endif;
+    }
 
 
-	/**
-	 * Gets any order pickup locations from the given order
-	 *
-	 * @since 1.8.0
-	 * @param \WC_Order $order the order
-	 * @return array of pickup locations, with country, postcode, state, city, address_2, address_1, company, phone, cost and id properties
-	 */
-	private function get_order_pickup_locations( $order ) {
+    /** Helper Methods ********************************************************/
 
-		$pickup_locations = array();
-		/** @type array|\WC_Order_Item_Shipping[] $shipping_methods */
-		$shipping_methods = $order->get_shipping_methods();
 
-		foreach ( $shipping_methods as $shipping_item ) {
+    /**
+     * Gets any order pickup locations from the given order
+     *
+     * @since 1.8.0
+     * @param \WC_Order $order the order
+     * @return array of pickup locations, with country, postcode, state, city, address_2, address_1, company, phone, cost and id properties
+     */
+    private function get_order_pickup_locations($order)
+    {
 
-			if ( SV_WC_Plugin_Compatibility::is_wc_version_gte_3_0() ) {
+        $pickup_locations = array();
+        /** @type array|\WC_Order_Item_Shipping[] $shipping_methods */
+        $shipping_methods = $order->get_shipping_methods();
 
-				$method_id       = $shipping_item->get_method_id();
-				$pickup_location = $shipping_item->get_meta( 'pickup_location', true );
+        foreach ($shipping_methods as $shipping_item) {
 
-			} else {
+            if (SV_WC_Plugin_Compatibility::is_wc_version_gte_3_0()) {
 
-				if ( ! isset( $shipping_item['pickup_location'] ) ) {
-					continue;
-				}
+                $method_id = $shipping_item->get_method_id();
+                $pickup_location = $shipping_item->get_meta('pickup_location', true);
 
-				$method_id       = $shipping_item['method_id'];
-				$pickup_location = $shipping_item['pickup_location'];
-			}
+            } else {
 
-			if ( WC_Local_Pickup_Plus::METHOD_ID === $method_id && ! empty( $pickup_location ) ) {
-				$pickup_locations[] = maybe_unserialize( $pickup_location );
-			}
-		}
+                if (!isset($shipping_item['pickup_location'])) {
+                    continue;
+                }
 
-		return $pickup_locations;
-	}
+                $method_id = $shipping_item['method_id'];
+                $pickup_location = $shipping_item['pickup_location'];
+            }
+
+            if (WC_Local_Pickup_Plus::METHOD_ID === $method_id && !empty($pickup_location)) {
+                $pickup_locations[] = maybe_unserialize($pickup_location);
+            }
+        }
+
+        return $pickup_locations;
+    }
 
 
 } // end \WC_Shipping_Local_Pickup_Plus_CPT class
